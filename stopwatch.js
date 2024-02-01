abbb505d1d52ae900eda5d9e25639197ebe9c072
Anirudh Konidala <68204671+kidskoding@users.noreply.github.com>
2024-01-31 22:39:43 -0600

    in the midst of having JS run the stopwatch

--- stopwatch.js
+++ stopwatch.js
@@ -1,4 +1,64 @@
-const startButton = document.getElementById('startButton')
-startButton.addEventListener('click', function() {
-    alert('Start button clicked!')
+document.addEventListener('DOMContentLoaded', function() {
+    const startButton = document.getElementById('startButton');
+    const stopwatchDisplay = document.querySelector('.stopwatch p strong');
+
+    let currentMilliseconds = 0;
+    let currentSeconds = 0;
+    let currentMinutes = 0;
+
+    let ticking = false;
+
+    let intervalTimer;
+
+    startButton.addEventListener('click', startStopwatch);
+    
+    function startStopwatch() {
+        if(!ticking) {
+            intervalTimer = setInterval(runMilliseconds, 10);
+
+            const runMilliseconds = () => {
+                currentMilliseconds++;
+                if(currentMilliseconds === 100) {
+                    currentMilliseconds = 0;
+                    runSeconds();
+                }
+                updateDisplay();
+            }
+    
+            const runSeconds = () => {
+                currentSeconds++;
+                if(currentSeconds === 60) {
+                    currentSeconds = 0;
+                    runMinutes();
+                }
+                updateDisplay();
+            }
+    
+            const runMinutes = () => {
+                currentMinutes++;
+            }
+    
+            const updateDisplay = () => {
+                const formattedTime = `${pad(currentMinutes)}:${pad(currentSeconds)}:${pad(currentMilliseconds)}`;
+                stopwatchDisplay.textContent = formattedTime;
+            }
+            startButton.addEventListener('click', stopStopwatch);
+
+            ticking = true;
+            startButton.textContent = 'Stop';
+            startButton.addEventListener('click', stopStopwatch);
+        } else {
+            stopStopwatch();
+        }
+    }
+
+    function stopStopwatch() {
+        clearInterval(intervalTimer);
+        ticking = false;
+        startButton.textContent = "Start";
+    }
+
+    function pad(value) {
+        return value < 10 ? "0" + value : value;
+    }
 });
