7d82239090a541c341a71079169678dcd6fe214b
Anirudh Konidala <68204671+kidskoding@users.noreply.github.com>
2024-02-19 19:38:53 -0600

    added functionality for lap button

--- stopwatch.js
+++ stopwatch.js
@@ -1,21 +1,37 @@
-let interval;
-let ticking = false;
-
 let currentMilliseconds = 0;
 let currentSeconds = 0;
 let currentMinutes = 0;
 
+let interval;
+let ticking = false;
+let lap = 1;
+
 document.addEventListener('DOMContentLoaded', function() {
     const startStopButton = document.getElementById('startStopButton');
+    const lapButton = document.getElementById('lapButton');
+
     startStopButton.addEventListener('click', function() {
         if(ticking) {
             clearInterval(interval);
             startStopButton.innerText = 'Start';
+            lapButton.innerText = 'Reset';
         } else {
             interval = setInterval(updateTime, 10);
             startStopButton.innerText = 'Stop';
+            lapButton.innerText = 'Lap';
         }
         ticking = !ticking
+    });
+
+    lapButton.addEventListener('click', function() {
+        if(ticking) {
+
+        } else {
+            currentMilliseconds = 0;
+            currentSeconds = 0;
+            currentMinutes = 0;
+            updateDisplay();
+        }
     });
 });
 
